02a8d4b6d82e257691e11e88245bf9795bc3506c
DragonSCP <135724825+DragonSCP@users.noreply.github.com>
2024-06-03 14:42:46 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,68 +1,63 @@
---Blox Fruit Gui2
-getgenv().SettingTheme = {
-    Tab = {
-        Left_Color = Color3.fromRGB(203 ,156, 255),
-        Right_Color = Color3.fromRGB(140, 166 ,252)
-    },
-    Button = {
-        Left_Color = Color3.fromRGB(203 ,156, 255),
-        Right_Color = Color3.fromRGB(140, 166 ,252)
-    },
-}
-if getgenv().SettingTheme == nil then
-	getgenv().SettingTheme = {
-    Tab = {
-        Left_Color = Color3.fromRGB(203 ,156, 255),
-        Right_Color = Color3.fromRGB(140, 166 ,252)
-    },
-    Button = {
-        Left_Color = Color3.fromRGB(203 ,156, 255),
-        Right_Color = Color3.fromRGB(140, 166 ,252)
-    },
-}
-    else
-local Hub = {
-     [5780309044] = {
-        [1] = "http://skyhubking.xyz/script/free_script/SKYHUB/StandAweaken.lua",
-   },
-     [6461766546] = {
-        [1] = "http://skyhubking.xyz/script/free_script/SKYHUB/AHeroDestiney.lua",
-   },
-     [1499872953] = {
-        [1] = "http://skyhubking.xyz/script/free_script/SKYHUB/BokuNo.lua",
-   },
-     [2753915549] = {
-        [1] = "http://skyhubking.xyz/script/free_script/SKYHUB/BloxFruit.lua",
-   },
-      [4442272183] = {
-        [1] = "http://skyhubking.xyz/script/free_script/SKYHUB/BloxFruit.lua",
-   },
-      [6110371797] = {
-        [1] = "http://skyhubking.xyz/script/free_script/SKYHUB/ProjectNewWorld.lua",
-   }, 
-      [6688650002] = {
-	[1] = "http://skyhubking.xyz/script/free_script/SKYHUB/RojutsuBlox.lua"
-   },
-      [3400631762] = {
-	[1] = "http://skyhubking.xyz/script/free_script/SKYHUB/JoJoBlox.lua"
-   },
-      [4313854107] = {
-	[1] = "http://skyhubking.xyz/script/free_script/SKYHUB/JoJoBlox.lua"
-   },
-      [4587545091] = {
-	[1] = "http://skyhubking.xyz/script/free_script/SKYHUB/VietNamPiece.lua"
-   },
-      [7148407314] = {
-	[1] = "http://skyhubking.xyz/script/free_script/SKYHUB/MagicSea.lua"
-   },
-}
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+local XRay = game.Workspace.XRay
 
-for i,v in pairs(Hub) do
-    if i == game.PlaceId then
-        print("CheckScript")
-           loadstring(game:HttpGet(v[1]))()
-        else
-        
-        end
-    end
-end
+local UIS = game:GetService("UserInputService")
+local Toggled = false
+local Button = Player.PlayerGui:WaitForChild("MainFrame"):WaitForChild("ToggleButton")
+local ValidKeyCode = {
+	Enum.KeyCode.E
+}
+local ValidTransparency = {}
+
+UIS.InputBegan:Connect(function(key, gameProcessedEvent)
+	if not gameProcessedEvent then
+		if not Toggled then
+			for _, KEY in pairs(ValidKeyCode) do
+				if key.KeyCode == KEY then
+					for partsIndex, parts in pairs(XRay:GetDescendants()) do
+						local success, err = pcall(function()
+							ValidTransparency["Part-"..parts.Name] = parts.Transparency
+							parts.Transparency = 0.5
+							Toggled = true
+						end)
+					end
+				end
+			end
+		else
+			for _, KEY in pairs(ValidKeyCode) do
+				if key.KeyCode == KEY then
+					for partsIndex, parts in pairs(XRay:GetDescendants()) do
+						local success, err = pcall(function()
+							parts.Transparency = ValidTransparency["Part-"..parts.Name]
+							Toggled = false
+						end)
+					end
+				end
+			end
+		end
+	end
+end)
+
+Button.MouseButton1Click:Connect(function()
+	if not Toggled then
+		for _, KEY in pairs(ValidKeyCode) do
+			for partsIndex, parts in pairs(XRay:GetDescendants()) do
+				local success, err = pcall(function()
+					ValidTransparency["Part-"..parts.Name] = parts.Transparency
+					parts.Transparency = 0.5
+					Toggled = true
+				end)
+			end
+		end
+	else
+		for _, KEY in pairs(ValidKeyCode) do
+			for partsIndex, parts in pairs(XRay:GetDescendants()) do
+				local success, err = pcall(function()
+					parts.Transparency = ValidTransparency["Part-"..parts.Name]
+					Toggled = false
+				end)
+			end
+		end
+	end
+end)
